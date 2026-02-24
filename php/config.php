529f64fcca9799ebeabfab021560b57551cb8d52
noahdevelopsio <noahbig001@gmail.com>
2026-02-24 04:51:15 +0100

    [INFRA] Add DB connection port and SSL support for Aiven

--- php/config.php
+++ php/config.php
@@ -13,6 +13,9 @@ $db_host = getenv("DB_HOST");
 $db_user = getenv("DB_USER");
 $db_pass = getenv("DB_PASS");
 $db_name = getenv("DB_NAME");
+$db_port = getenv("DB_PORT") ? (int)getenv("DB_PORT") : 3306;
 
-$con = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("Couldn't connect");
+$con = mysqli_init();
+// Ensure SSL connection is utilized if supported/required by the remote server (e.g. Aiven)
+mysqli_real_connect($con, $db_host, $db_user, $db_pass, $db_name, $db_port, NULL, MYSQLI_CLIENT_SSL) or die("Couldn't connect: " . mysqli_connect_error());
 ?>
